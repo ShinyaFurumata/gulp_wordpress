babde2121b03a01bf3db79a3d792968879590d62
Shinya Furumata <shin0724ya@gmail.com>
2016-07-21 22:51:33 +0900

    gulp-slimのインストール,phpのコンパイルテスト

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require("gulp");
 var browser = require("browser-sync");
 var plumber = require("gulp-plumber");
+var slim = require("gulp-slim");
 
 gulp.task("server", function() {
     browser({
@@ -18,3 +19,14 @@ gulp.task('watch', function () {
 });
 
 gulp.task("default", ['server' , 'watch'] );
+
+/*--------------------- slim [slim] --------------------*/
+gulp.task('slim', function() {
+  gulp.src("./app/views/**/*.slim")
+    .pipe(plumber())
+    .pipe(slim({
+      pretty: true,
+    }))
+    .pipe(gulp.dest('./www/wordpress/wp-content/themes/sample_theme/'))
+    .pipe(browser.reload({stream:true}));
+});
